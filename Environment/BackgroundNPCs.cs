e64393107412dcf6f9c08afde0befae7228e2953
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-10-01 00:21:49 +0300

    Adding static npcs

---
+++ Environment/BackgroundNPCs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Environment
+{
+    public class BackgroundNPCs : MonoBehaviour
+    {
+        private Animator animator;
+        [SerializeField] string[] animations;
+        [SerializeField] int animationToPlay = 0;
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+        private void Start()
+        {
+            animator.Play(animations[animationToPlay]);
+        }
+    }
+}
