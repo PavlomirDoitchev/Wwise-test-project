9f3f9a48c0691bc95379cf87f941d2ea290385ed
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-10-09 23:27:37 +0300

    Improving the dash state

--- StateMachine/Player/States/PlayerDashState.cs
+++ StateMachine/Player/States/PlayerDashState.cs
@@ -1,58 +1,68 @@
-﻿using Assets.Scripts.Utilities.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Assets.Scripts.StateMachine.Player.States;
+using Assets.Scripts.StateMachine.Player;
+using Assets.Scripts.Utilities.Contracts;
 using UnityEngine;
-namespace Assets.Scripts.StateMachine.Player.States
+
+public class PlayerDashState : PlayerBaseState
 {
-    public class PlayerDashState : PlayerBaseState
+    private float duration;
+    private float lastGroundedTime;
+    private const float groundedBuffer = 0.15f; 
+
+    public PlayerDashState(PlayerStateMachine stateMachine, float duration) : base(stateMachine)
     {
-        float duration;
-        public PlayerDashState(PlayerStateMachine stateMachine, float duration) : base(stateMachine)
-        {
-            this.duration = duration;
-        }
+        this.duration = duration;
+    }
 
-        public override void Enter()
-        {
+    public override void Enter()
+    {
+        if (_playerStateMachine.CharacterController.isGrounded)
+            lastGroundedTime = Time.time;
+
+        bool groundedBuffered = Time.time - lastGroundedTime <= groundedBuffer;
+
+        if (groundedBuffered)
+            _playerStateMachine.Animator.CrossFadeInFixedTime("Slide", 0.1f);
+        else
             _playerStateMachine.Animator.CrossFadeInFixedTime("Dash", 0.1f);
-            _playerStateMachine.ForceReceiver.SetForce(_playerStateMachine.transform.forward * _playerStateMachine.PlayerStats.DashForce);
+        _playerStateMachine.ForceReceiver.SetForce(
+            _playerStateMachine.transform.forward * _playerStateMachine.PlayerStats.DashForce);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        if (_playerStateMachine.CharacterController.isGrounded)
+            lastGroundedTime = Time.time;
+        _playerStateMachine.ForceReceiver.verticalVelocity = 0f;
+
+        duration -= deltaTime;
+        if (duration <= 0f)
+        {
+            _playerStateMachine.ChangeState(new PlayerLocomotionState(_playerStateMachine));
+            return;
         }
 
-       
+        Move(deltaTime);
+    }
 
-        public override void Tick(float deltaTime)
+    public override void Exit()
+    {
+        _playerStateMachine.ForceReceiver.SetForce(Vector3.zero);
+    }
+
+    // Collider logic unchanged
+    public override void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.TryGetComponent<IPushable>(out _)) return;
+
+        if (hit.gameObject.TryGetComponent<INotPushable>(out INotPushable notPushable))
         {
-            
-            _playerStateMachine.ForceReceiver.verticalVelocity = 0f;
-            duration -= deltaTime;
-            if (duration <= 0f)
+            if (Mathf.Abs(hit.normal.y) < 0.1f)
             {
-                _playerStateMachine.ChangeState(new PlayerLocomotionState(_playerStateMachine));
-                return;
-            }
-            Move(deltaTime);
-        }
+                if (hit.normal.x > 0) _playerStateMachine.currentLeftBlocker = notPushable;
+                if (hit.normal.x < 0) _playerStateMachine.currentRightBlocker = notPushable;
 
-        public override void Exit()
-        {
-            _playerStateMachine.ForceReceiver.SetForce(Vector3.zero);
-        }
-        public override void OnControllerColliderHit(ControllerColliderHit hit)
-        {
-            if (hit.gameObject.TryGetComponent<IPushable>(out _)) return;
-
-            if (hit.gameObject.TryGetComponent<INotPushable>(out INotPushable notPushable))
-            {
-                if (Mathf.Abs(hit.normal.y) < 0.1f)
-                {
-                    if (hit.normal.x > 0) _playerStateMachine.currentLeftBlocker = notPushable;
-                    if (hit.normal.x < 0) _playerStateMachine.currentRightBlocker = notPushable;
-
-                    _playerStateMachine.ChangeState(new PlayerCollisionState(_playerStateMachine, 0.2f));
-                }
+                _playerStateMachine.ChangeState(new PlayerCollisionState(_playerStateMachine, 0.2f));
             }
         }
     }
