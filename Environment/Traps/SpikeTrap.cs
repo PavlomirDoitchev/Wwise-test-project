5c66a21a7bce0a7fb0759393666b18643c9b4efb
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-10-26 10:16:33 +0200

    Improving traps

--- Environment/Traps/SpikeTrap.cs
+++ Environment/Traps/SpikeTrap.cs
@@ -1,17 +1,65 @@
-﻿
+﻿using Assets.Scripts.Utilities.Contracts;
+using System.Collections;
 using UnityEngine;
+
 namespace Assets.Scripts.Environment.Traps
 {
     public class SpikeTrap : Trap
     {
-        private BoxCollider boxCollider;
-        private void Awake()
+        // TODO: Create a new layer for traps and trap triggers. Make sure they cannot interact with each other!
+        [Tooltip("If true, animation will loop, else trggier upon something landing on trap")]
+        [SerializeField] bool isAutomatic;
+        [SerializeField] float timeToActivate = 1f;
+        [SerializeField] private float timer; 
+        [SerializeField] private BoxCollider triggerCollider;
+        private Animator animator;
+        private readonly int Idle = Animator.StringToHash("Idle");
+        private readonly int Trigger = Animator.StringToHash("TFD_Floor_Trap_01A");
+        bool hasEnteredTrigger = false;
+        private void Start()
         {
-            boxCollider = GetComponent<BoxCollider>();
+            if (triggerCollider == null)
+                triggerCollider = GetComponentInChildren<BoxCollider>();
+            animator = GetComponent<Animator>();
+            if (isAutomatic)
+                animator.Play(Trigger);
+            else
+                animator.Play(Idle);
+
+            timer = timeToActivate;
+
         }
-        public void EnableCollider() 
+        private void OnTriggerEnter(Collider other)
         {
-            boxCollider.enabled = !boxCollider.enabled;   
+            if (isAutomatic) return;
+            if (other == triggerCollider) return;
+            if (other.TryGetComponent<IDamagable>(out var _))
+                hasEnteredTrigger = true;
         }
+        private void Update()
+        {
+            if (hasEnteredTrigger && !isAutomatic)
+            {
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    animator.Play(Trigger);
+                    hasEnteredTrigger = false;
+                }
+            }
+        }
+     
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent<IDamagable>(out var damagable) && !isAutomatic)
+            {
+                hasEnteredTrigger = false;
+                timer = timeToActivate; 
+            }
+
+        }
+
+        public void EnableDamageCollider() => triggerCollider.enabled = true;
+        public void DisableDamageCollider() => triggerCollider.enabled = false;
     }
 }
--- Environment/Traps/Trap.cs
+++ Environment/Traps/Trap.cs
@@ -6,12 +6,21 @@ namespace Assets.Scripts.Environment.Traps
     public class Trap : MonoBehaviour
     {
         [SerializeField] int damage;
+        [SerializeField] Collider damageCollider;
+       
         private void OnTriggerEnter(Collider collision)
         {
             if (collision.gameObject.TryGetComponent(out IDamagable damagable)) 
             {
                 damagable.TakeDamage(damage);
             }
+        }
+        /// <summary>
+        /// Used in animation events
+        /// </summary>
+        public void EnableCollider()
+        {
+            damageCollider.enabled = !damageCollider.enabled;
         }
     }
 }
